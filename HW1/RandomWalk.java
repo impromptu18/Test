299aada824f4fa58536a0e5b90844aa4c01cdda0
Nhi <yennhi.vt06@gmail.com>
2025-09-24 15:25:38 +0700

    submit HW1

---
+++ HW1/RandomWalk.java
@@ -0,0 +1,66 @@
+/******************************************************************************
+ *  Compilation:  javac RandomWalk.java
+ *  Execution:    java RandomWalk n
+ *  Dependencies: StdDraw.java
+ *
+ *  % java RandomWalk 20
+ *  total steps = 300
+ *
+ *  % java RandomWalk 50
+ *  total steps = 2630
+ *
+ *  Simulates a 2D random walk and plots the trajectory.
+ *
+ *  Remarks: works best if n is a divisor of 600.
+ *
+ ******************************************************************************/
+
+public class RandomWalk {
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        StdDraw.setScale(-n - 0.5, n + 0.5);
+        StdDraw.clear(StdDraw.GRAY);
+        StdDraw.enableDoubleBuffering();
+
+        int x = 0, y = 0;
+        int steps = 0;
+        int direction = 0;
+        while (Math.abs(x) < n && Math.abs(y) < n) {
+            StdDraw.setPenColor(StdDraw.WHITE);
+            StdDraw.filledSquare(x, y, 0.45);
+            if (direction == 0){
+                if (x == y+1) {
+                    direction = 1;
+                    continue;
+                }
+                x++;
+            } else if (direction == 1) {
+                if (y == -x) {
+                    direction = 2;
+                    continue;
+                }
+                y--;
+            } else if (direction == 2) {
+                if (x == y) {
+                    direction = 3;
+                    continue;
+                }
+                x--;
+            } else if (direction == 3) {
+                if (y == -x) {
+                    direction = 0;
+                    continue;
+                }
+                y++;
+            }
+            steps++;
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.filledSquare(x, y, 0.45);
+            StdDraw.show();
+            StdDraw.pause(40);
+        }
+        StdOut.println("Total steps = " + steps);
+    }
+
+}
